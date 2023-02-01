6a9d83c14d26d85117a0782eef79d3e4fd046bfa
Oleg <bombastik@ukr.net>
2023-02-01 20:26:17 +0200

    centry

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,23 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from "@sentry/angular";
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+Sentry.init({
+  dsn: "https://8423703fc54b428c892522dfd0e3a6ff@o4504606125064192.ingest.sentry.io/4504606127554560",
+  integrations: [
+    new BrowserTracing({
+      tracePropagationTargets: ["localhost", "https://yourserver.io/api"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
 
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 if (environment.production) {
   enableProdMode();
 }
